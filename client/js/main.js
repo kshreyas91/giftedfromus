14cbddf1e214b58092eee7bcbdf98b0c2476a940
Shreyas <ssk272@cornell.edu>
2015-10-14 15:12:42 -0400

    Adding GiftSuggestion Model

--- client/js/main.js
+++ client/js/main.js
@@ -7,6 +7,7 @@ this.App = {};
 
 // Create the needed collections on the client side
 this.Surprises = new Meteor.Collection("surprises");
+this.GiftSuggestions = new Meteor.Collection("giftsuggestions")
 
 // Subscribe to the publishes in server/collections
 
