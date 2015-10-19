1f4bfca756a6229947b55b450df003cef249cdf8
admin <admin@admins-MacBook-Pro.local>
2015-10-19 18:18:57 -0400

    Sidebar, main page

--- client/js/views/login.js
+++ client/js/views/login.js
@@ -1,8 +1,12 @@
-Template.login.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var emailVar = event.target.loginEmail.value;
-        var passwordVar = event.target.loginPassword.value;
-        Meteor.loginWithPassword(emailVar, passwordVar);
-    }
+Meteor.startup(function() {
+    Template.login.events = {
+        'submit form': function(event){
+            event.preventDefault();
+            var emailVar = event.target.loginEmail.value;
+            var passwordVar = event.target.loginPassword.value;
+            Meteor.loginWithPassword(emailVar, passwordVar);
+            Router.go('home');
+                $('#logIn').modal('hide');
+            }
+    };    
 });
--- client/js/views/register.js
+++ client/js/views/register.js
@@ -1,5 +1,5 @@
 Template.register.events({
-    'submit form': function(event) {
+    'submit .registration-form': function(event) {
         event.preventDefault();
         var emailVar = event.target.registerEmail.value;
         var passwordVar = event.target.registerPassword.value;
@@ -10,5 +10,9 @@ Template.register.events({
             profile: {name: name}
         });
        // Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.name":name}})
+        Meteor.loginWithPassword(emailVar, passwordVar);
+        Router.go('home')
     }
+
+    
 });
