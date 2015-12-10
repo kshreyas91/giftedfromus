a7e7b68397b6fdb13248c5791d17f58089ef3fc3
franksli <franksli@gmail.com>
2015-12-10 16:40:23 -0500

    Update Emailing

--- client/js/views/createNewSurprise.js
+++ client/js/views/createNewSurprise.js
@@ -2,6 +2,7 @@
  * View logic for the createNewSurprise page
  */
 
+
 Meteor.startup(function() {
 
   Template.createNewSurprise.events = {
@@ -17,6 +18,8 @@ Meteor.startup(function() {
 
       var id= Surprises.insert({title: formCampaignTitle, name: formGifteeName, description: formCampaignDesc, date: formCampaignDate, pledgeGoal: formPledgeGoal, endDate: formCampaignEndDate, createdBy: createdBy});
       //document.getElementById('showPublicURL').innerHTML = (location.origin + "/campaign/" +id);
+      Meteor.call('sendEmail',Meteor.user().emails[0].address,formCampaignTitle);
+      //Email.send({to:'franksli@gmail.com', from:'franksli@gmail.com', subject:'Thank you for signing up for our project', text:'We will share with you some news about us in a near future. See you soon!'});
       window.location.href = (location.origin + "/campaign/" +id);
     },
   };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 /*
  * The main server file, general server side code should go here
  */
+Meteor.startup(function () {
+ process.env.MAIL_URL = 'smtp://postmaster%40sandbox83585ee904b441b1852a714b7a53d33d.mailgun.org:85535883b148234c4d00a8f4c51e43e7@smtp.mailgun.org:587';
+ //Email.send({to:'franksli@gmail.com', from:'franksli@gmail.com', subject:'1Thank you for signing up for our project', text:'We will share with you some news about us in a near future. See you soon!'});
+});
+
+  // on the server, we create the sendEmail RPC function
+  Meteor.methods({
+    sendEmail: function(email, name) {
+      // send the email!
+      var subject = 'Your Campaign '+name+' has been created';
+       Email.send({to:email, from:'franksli@gmail.com', subject: subject, text:'We will share with you some news about us in a near future. See you soon!'});
+	}
+  });
 
 // Set permissions on the users collection
 Meteor.users.allow({
@@ -10,3 +23,4 @@ Meteor.users.allow({
     return userId == this.userId
   }
 });
+
